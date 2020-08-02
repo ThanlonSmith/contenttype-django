a2cd2cfe2fc05b5a83a28361c4b5313b84baea25
erics1996 <erics1996@yeah.net>
2020-08-02 12:56:29 +0800

    使用常规的方式创建一条课程价格策略。

--- app/views.py
+++ app/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
+from app import models
+
 
 # Create your views here.
+def test(request):
+    """
+    # 添加之前向两个课程表中添加数据
+    models.Course.objects.create(name='Python')
+    models.Course.objects.create(name='Flask')
+    models.Course.objects.create(name='Django')
+    models.VipCourse.objects.create(name='DRF')
+    models.VipCourse.objects.create(name='VUE')
+    models.VipCourse.objects.create(name='Scrapy')
+    """
+    """
+    添加一条普通课程为“Python”价格为99.9，学习周期为3个月的价格策略记录()
+    """
+    course_obj = models.Course.objects.filter(name='Python').first()
+    print(course_obj)  # Course object (1)
+    # 首先需要获取根据课程的名称获取课程的id
+    course_id = course_obj.id
+    # 然后还要获取content_type表中course表对应的id
+    content_type_obj = models.ContentType.objects.filter(model='course').first()
+    content_type_id = content_type_obj.id
+    models.PricePolicy.objects.create(price=99.9, period=3, course_id=course_id, content_type_id=content_type_id)
+    return HttpResponse('...')
